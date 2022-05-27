1bb7e41b7698e698dcc2b56e088abe74768dddf0
abel <2698922550@qq.com>
2022-05-28 00:17:01 +0800

    define server and mux

--- src/web02/main.go
+++ src/web02/main.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type MyHandler struct{}
 
 func (m *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprint(w, "通过自己创建的处理器处理请求")
+	// fmt.Fprint(w, "通过自己创建的处理器处理请求")
+	fmt.Fprint(w, "通过自己配置服务器信息处理请求")
 }
 
 func main() {
 
-	MyHandler := MyHandler{}
+	myHandler := MyHandler{}
 
-	http.Handle("/myhandler", &MyHandler)
+	// http.Handle("/myhandler", &MyHandler)
+	// 创建server结构
+	server := http.Server{
+		Addr:        ":4396",
+		Handler:     &myHandler,
+		ReadTimeout: 2 * time.Second,
+	}
 
 	//创建路由
-	http.ListenAndServe(":4396", nil)
+	//http.ListenAndServe(":4396", nil)
+	server.ListenAndServe()
 }
--- src/webapp/main.go
+++ src/webapp/main.go
@@ -7,13 +7,16 @@ import (
 
 // 创建处理器函数
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!", r.URL.Path)
+	// fmt.Fprintln(w, "Hello World!", r.URL.Path)
+	fmt.Fprintln(w, "通过自己创建的多路复用器创建请求!", r.URL.Path)
 }
 
 func main() {
-
-	http.HandleFunc("/", handler)
+	// 创建多路复用器
+	mux := http.NewServeMux()
+	// http.HandleFunc("/", handler)
+	mux.HandleFunc("/", handler)
 
 	//创建路由
-	http.ListenAndServe(":4396", nil)
+	http.ListenAndServe(":4396", mux)
 }
